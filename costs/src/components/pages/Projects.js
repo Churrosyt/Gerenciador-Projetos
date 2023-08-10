5c85d46a3dd8ac9934096381609191dd93338548
Churros <klebersonluiz5@gmail.com>
2023-08-10 19:06:11 -0300

    Criando o componente loader Loading

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -5,12 +5,14 @@ import { useState, useEffect } from "react";
 import Message from "../layout/Message";
 
 import Container from "../layout/Container";
+import Loading from "../layout/Loading";
 import LinkButton from "../layout/LinkButton";
 import styles from "./Projects.module.css";
 import ProjectCard from "../project/ProjectCard";
 
 function Projects() {
     const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false); // deixar como false pq o loading sempre inicia
 
     const location = useLocation(); // resgatar a mensagem, message
     console.log(location);
@@ -21,7 +23,8 @@ function Projects() {
     }
 
     useEffect(() => {
-        fetch("http://localhost:5000/projects", {
+        setTimeout(() => {
+            fetch("http://localhost:5000/projects", {
             metho: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -31,8 +34,10 @@ function Projects() {
             .then((data) => {
                 console.log(data);
                 setProjects(data);
+               setRemoveLoading(true); // quando carregar todos os projetos, mudar o para true
             })
             .catch((err) => console.log(err));
+        }, 500)
     }, []);
 
     return (
@@ -42,6 +47,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
+             
             <Container customClass="start">
                 {projects.length > 0 && // checagem SE eu tenho produtos // Project.leght maior de 0, pode prosseguir
                     projects.map((project) => (
@@ -53,6 +59,11 @@ function Projects() {
                             key={project}
                         />
                     ))}
+                   {!removeLoading && <Loading/> /*Para mostrar o loading*/}  
+                   {removeLoading && projects.length === 0 &&( // se caso nao tiver nenhum projeto, ele mostra a mensagem
+                   <p>Não há projetos cadastrado</p>
+                   )}
+                
             </Container>
         </div> //se o message esta preechido, vamos imprimir nossa mensagem do "Message"
     );
