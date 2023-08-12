23003f1a63dcb76c5f457b425ef3a7d51f59d87b
Churros <klebersonluiz5@gmail.com>
2023-08-12 11:51:14 -0300

    Removendo projetos

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -13,6 +13,7 @@ import ProjectCard from "../project/ProjectCard";
 function Projects() {
     const [projects, setProjects] = useState([]);
     const [removeLoading, setRemoveLoading] = useState(false); // deixar como false pq o loading sempre inicia
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation(); // resgatar a mensagem, message
     console.log(location);
@@ -25,20 +26,37 @@ function Projects() {
     useEffect(() => {
         setTimeout(() => {
             fetch("http://localhost:5000/projects", {
-            metho: "GET",
+                metho: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data);
+                    setProjects(data);
+                    setRemoveLoading(true); // quando carregar todos os projetos, mudar o para true
+                })
+                .catch((err) => console.log(err));
+        }, 500);
+    }, []);
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
             },
         })
-            .then((resp) => resp.json())
-            .then((data) => {
-                console.log(data);
-                setProjects(data);
-               setRemoveLoading(true); // quando carregar todos os projetos, mudar o para true
+            .then((resp) => resp.json()) // transformar em json
+            .then((data) => { // pegar os dados da requisicao
+                setProjects(projects.filter((project) => project.id !== id));
+                //fazendo um map de Projects, percorrendo cada um dos projetos
+                // project.id !== id) para filtrar e setar uma nova lista sem o projeto que nos deletamos
+                setProjectMessage('Projeto removido com sucesso!')
             })
-            .catch((err) => console.log(err));
-        }, 500)
-    }, []);
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
@@ -47,7 +65,8 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
-             
+            {projectMessage && <Message type="success" msg={projectMessage} />}
+
             <Container customClass="start">
                 {projects.length > 0 && // checagem SE eu tenho produtos // Project.leght maior de 0, pode prosseguir
                     projects.map((project) => (
@@ -57,13 +76,14 @@ function Projects() {
                             budget={project.budget}
                             category={project.category.name}
                             key={project}
+                            handleRemove={removeProject}
                         />
                     ))}
-                   {!removeLoading && <Loading/> /*Para mostrar o loading*/}  
-                   {removeLoading && projects.length === 0 &&( // se caso nao tiver nenhum projeto, ele mostra a mensagem
-                   <p>Não há projetos cadastrado</p>
-                   )}
-                
+                {!removeLoading && <Loading /> /*Para mostrar o loading*/}
+                {removeLoading &&
+                    projects.length === 0 && ( // se caso nao tiver nenhum projeto, ele mostra a mensagem
+                        <p>Não há projetos cadastrado</p>
+                    )}
             </Container>
         </div> //se o message esta preechido, vamos imprimir nossa mensagem do "Message"
     );
