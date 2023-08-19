45d926794484de80099dbbb38a4be7dfcedce1e8
Churros <klebersonluiz5@gmail.com>
2023-08-19 00:28:44 -0300

    Adicionando novos serviços

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -1,3 +1,5 @@
+import {parse, v4 as uuidv4} from 'uuid'
+
 import styles from "./Project.module.css";
 
 import { useParams } from "react-router-dom"; // para pegarmos o id pela url
@@ -9,6 +11,7 @@ import Container from "../layout/Container";
 
 import Message from "../layout/Message";
 import ProjectForm from "../project/ProjectForm";
+import ServiceForm from "../Service/ServiceForm";
 
 function Project() {
     const { id } = useParams(); // ele sabe que iremos pegar o id a partir da URL
@@ -62,6 +65,45 @@ function Project() {
             setType('success')
         })
         .catch(err => console.log(err))
+    }
+
+    function createService(project) {
+        setMessage('')
+
+       // last service
+       const lastService = project.services[project.services.length - 1] 
+
+       lastService.id = uuidv4()
+
+       const lastServiceCost = lastService.cost
+
+       const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+       // maximum value validation
+       if(newCost > parseFloat(project.budget)){
+        setMessage("Orçamento ultrapassado, verifique o valor do serviço")
+        setType("erro")
+        project.services.pop()
+        return false
+       }
+
+       // add service cost to project cost
+       project.cost = newCost
+
+       // update project
+       fetch(`http://localhost:5000/projects/${project.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(project)
+       }).then((resp) => resp.json())
+       .then(data => {
+        // exibir os serviços
+        console.log(data)
+       })
+       .catch(err => console.log(err))
+
     }
 
     function toggleProjectForm() {
@@ -125,7 +167,13 @@ function Project() {
                                cas o contrario eu exibo fechar */}
                             </button>
                             <div className={styles.project_info}>
-                               {showServiceForm && <div>formulário do serviço</div> }
+                               {showServiceForm && (
+                                <ServiceForm
+                                handleSubmit={createService}
+                                btnText="Adicionar serviço"
+                                projectData={project}
+                                />
+                               ) }
                                {/* se showServiceForm estiver abilitado, abilite o formulário do serviço */}
                             </div>
                         </div>
@@ -133,7 +181,7 @@ function Project() {
                         <Container customClass="start">
                                <p>Itens de serviço</p>
                         </Container>
-                    </Container>
+                    </Container> 
                 </div>
             ) : (
                 <Loading />
